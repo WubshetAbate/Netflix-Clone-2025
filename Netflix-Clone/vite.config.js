d005f36e723fe5890a9ff2fe00371042e599f625
wube0965 <wubshetabate865@gmail.com>
2025-09-11 18:10:34 +0300

    deploy setup

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Netflix-Clone-2025/", // 👈 VERY IMPORTANT
   plugins: [react()],
-})
+});
