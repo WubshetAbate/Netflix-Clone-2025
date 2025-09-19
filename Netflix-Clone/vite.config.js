aedb5d2d993d46b14d166ed5896480dcbafca81e
wube0965 <wubshetabate865@gmail.com>
2025-09-19 18:45:21 +0300

    update

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2025/",
+  base: "Netflix-Clone-2025",
+  build: {
+    outDir: "dist",
+  },
 });
